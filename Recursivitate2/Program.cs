dc4b6e66ec5b30f334387ff59fe648b106205ac4
Amalia Andreea Gurau <amalia.gurau@emmanuel.ro>
2022-01-13 15:58:39 +0200

    recursiv2

---
+++ Recursivitate2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 6, 7, 8, 9, 10 };
+            Console.WriteLine(Recursivitate.Maxim(vector, vector.Length - 1, int.MinValue));
+        }
+    }
+}
